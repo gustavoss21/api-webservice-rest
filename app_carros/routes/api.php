c4975e7a7754677ca4a5fe99533b359b4c04ec23
gustavo <gustavosantossouza65@gmail.com>
2023-10-08 10:41:50 -0300

    criando as rotas da api

--- app_carros/routes/api.php
+++ app_carros/routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\LocacaoController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ModeloController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +23,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('carro',CarroController::class);
+Route::apiResource('cliente',ClienteController::class);
+Route::apiResource('locacao',LocacaoController::class);
+Route::apiResource('marca',MarcaController::class);
+Route::apiResource('modelo',ModeloController::class);
