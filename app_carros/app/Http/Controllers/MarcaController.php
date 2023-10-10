5aa680bd2fe6aacd4bbf07a85f45fc2e9d2fcc41
gustavo <gustavosantossouza65@gmail.com>
2023-10-10 05:57:56 -0300

    apresentando registros de marca via ger

--- app_carros/app/Http/Controllers/MarcaController.php
+++ app_carros/app/Http/Controllers/MarcaController.php
@@ -14,7 +14,8 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        $marca = Marca::all();
+        return $marca;
     }
 
     /**
@@ -48,7 +49,7 @@ class MarcaController extends Controller
      */
     public function show(Marca $marca)
     {
-        //
+        return $marca;
     }
 
     /**
