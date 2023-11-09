6f52d8d75a3d826d1c2c0ec6f1a638cc63fe39f7
gustavo <gustavosantossouza65@gmail.com>
2023-11-09 16:36:46 -0300

    adicionando beacrumb

--- app_carros/resources/views/layouts/app.blade.php
+++ app_carros/resources/views/layouts/app.blade.php
@@ -90,6 +90,13 @@
         </nav>
 
         <main class="py-4">
+
+            <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{Route::currentRouteName()}}</li>
+            </ol>
+            </nav>
             @yield('content')
         </main>
     </div>
